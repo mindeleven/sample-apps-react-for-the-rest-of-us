09ae6df808874783a0def7f950704fca36585c63
Jürgen Kober <mindeleven@posteo.de>
2021-01-28 12:34:40 +0100

    Adding some scaffolding to first sample project

---
+++ complex-react-app/app/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function ExampleComponent() {
+  return (
+    <div>
+      <h1>This is our app!</h1>
+      <p>The sky is blue.</p>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <ExampleComponent />,
+  document.querySelector('#app')
+);
