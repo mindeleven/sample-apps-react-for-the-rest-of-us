5e6cf3bbf5ca076da399ef47b66b2599b83ba745
Jürgen Kober <mindeleven@posteo.de>
2021-04-01 16:04:20 +0200

    Modifying the HTML output of the container component

--- complex-react-app/app/components/Container.js
+++ complex-react-app/app/components/Container.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 
 function Container(props) {
   return (
-    <div className="container container--narrow py-md-5">
+    <div className={"container py-md-5" + (props.wide ? "" : " container--narrow")}>
       {props.children}
     </div>
   );
