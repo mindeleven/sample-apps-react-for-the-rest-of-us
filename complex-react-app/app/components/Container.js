9e74ca9e89667a9ade8b300413a946aa74302138
Jürgen Kober <mindeleven@posteo.de>
2021-04-01 15:21:59 +0200

    Modifying the container component to display props.children

---
+++ complex-react-app/app/components/Container.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+function Container(props) {
+  return (
+    <div className="container container--narrow py-md-5">
+      {props.children}
+    </div>
+  );
+}
+
+export default Container;
